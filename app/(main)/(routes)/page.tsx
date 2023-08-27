00ec64469c40bda2066743828c2cff37c465c1c6
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-08-27 18:53:16 -0400

    feat(backend): added authentication with clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,6 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
+import { UserButton } from '@clerk/nextjs'
 export default function Home() {
-	return <div>This is a protected route</div>
+	return (
+		<div>
+			This is a protected route
+			<UserButton afterSignOutUrl='/' />
+		</div>
+	)
 }
