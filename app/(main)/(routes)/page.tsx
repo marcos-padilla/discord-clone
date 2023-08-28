59b1c305f5bac82e155541abeabec606fc76c282
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-08-27 21:49:34 -0400

    feat(fronted): added dark/light theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ToggleTheme } from '@/components/ToggleTheme'
 import { UserButton } from '@clerk/nextjs'
 export default function Home() {
 	return (
 		<div>
 			This is a protected route
 			<UserButton afterSignOutUrl='/' />
+			<ToggleTheme />
 		</div>
 	)
 }
