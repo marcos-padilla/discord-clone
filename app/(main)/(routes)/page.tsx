78e3999f3c5fb24dad0c1190d7f45f97aa9c8fce
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-08-27 21:52:36 -0400

    fix(fronted): :art: system theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,7 +3,6 @@ import { UserButton } from '@clerk/nextjs'
 export default function Home() {
 	return (
 		<div>
-			This is a protected route
 			<UserButton afterSignOutUrl='/' />
 			<ToggleTheme />
 		</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 					<ThemeProvider
 						attribute='class'
 						defaultTheme='dark'
-						enableSystem={false}
+						enableSystem
 						storageKey='discord-theme'
 					>
 						{children}
