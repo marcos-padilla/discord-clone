ba84bc742f19534965da677b44d235d7310591b7
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-08-29 11:07:03 -0400

    feat: added functionality to InitialModal

--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -24,6 +24,9 @@ import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useEffect, useState } from 'react'
+import FileUpload from '../FileUpload'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const formSchema = z.object({
 	name: z.string().min(1, { message: 'Server name is required' }),
@@ -32,7 +35,7 @@ const formSchema = z.object({
 
 export default function InitialModal() {
 	const [isMounted, setIsMounted] = useState(false)
-
+	const router = useRouter()
 	useEffect(() => {
 		setIsMounted(true)
 	}, [])
@@ -47,8 +50,14 @@ export default function InitialModal() {
 
 	const isLoading = form.formState.isSubmitting
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		console.log(values)
-		console.log('asdasdasd')
+		try {
+			await axios.post('/api/servers', values)
+			form.reset()
+			router.refresh()
+			window.location.reload()
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
 	if (!isMounted) return null
@@ -72,7 +81,25 @@ export default function InitialModal() {
 					>
 						<div className='space-y-8 px-6'>
 							<div className='flex items-center justify-center text-center'>
-								TODO: Image Upload
+								<FormField
+									control={form.control}
+									name='imageUrl'
+									render={({ field }) => (
+										<FormItem>
+											<FormControl>
+												<FileUpload
+													endpoint='serverImage'
+													value={
+														field.value
+													}
+													onChange={
+														field.onChange
+													}
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
 							</div>
 							<FormField
 								control={form.control}
