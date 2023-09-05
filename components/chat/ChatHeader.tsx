8a499076a1d3bd35928e68cbe48c9c6bd0cb5318
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-09-05 15:49:00 -0400

    feat(fronted): added badge indicator for socket connection

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react'
 import MobileToggle from '../MobileToggle'
 import UserAvatar from '../UserAvatar'
+import SocketIndicator from '../SocketIndicator'
 
 interface ChatHeaderProps {
 	serverId: string
@@ -32,6 +33,9 @@ export default function ChatHeader({
 			<p className='font-semibold text-md text-black dark:text-white'>
 				{name}
 			</p>
+			<div className='ml-auto flex items-center'>
+				<SocketIndicator />
+			</div>
 		</div>
 	)
 }
