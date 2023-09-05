d18af4ddbfb36f3edc22ed64de7608ee16374138
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-09-05 11:58:24 -0400

    feat(fronted): added chat header

--- app/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,52 @@
-export default function ChannelIdPage() {
-	return <div>ChannelIdPage</div>
+import { getCurrentProfile } from '@/lib/current-profile'
+import { redirectToSignIn } from '@clerk/nextjs'
+import prisma from '@/lib/db'
+import { redirect } from 'next/navigation'
+import ChatHeader from '@/components/chat/ChatHeader'
+
+interface ChannelIdPageProps {
+	params: {
+		serverId: string
+		channelId: string
+	}
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+	const profile = await getCurrentProfile()
+	if (!profile) {
+		return redirectToSignIn()
+	}
+
+	const channel = await prisma.channel.findUnique({
+		where: {
+			id: params.channelId,
+			server: {
+				members: {
+					some: {
+						profileId: profile.id,
+					},
+				},
+			},
+		},
+	})
+
+	const member = await prisma.member.findFirst({
+		where: {
+			serverId: params.serverId,
+			profileId: profile.id,
+		},
+	})
+
+	if (!channel || !member) {
+		return redirect('/')
+	}
+	return (
+		<div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+			<ChatHeader
+				name={channel.name}
+				serverId={channel.serverId}
+				type='channel'
+			/>
+		</div>
+	)
 }
