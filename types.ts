eabbfdb49f9320874fabf4898ccf8e6b3ae012b7
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-09-05 15:28:37 -0400

    feat: added new NextApiResponse type for sockets

--- types.ts
+++ types.ts
@@ -1,6 +1,17 @@
 import { Member, Profile, Server, Channel } from '@prisma/client'
+import { Server as NetServer, Socket } from 'net'
+import { NextApiResponse } from 'next'
+import { Server as SocketIOServer } from 'socket.io'
 
 export type ExtendedServer = Server & {
 	members: (Member & { profile: Profile })[]
 	channels: Channel[]
+}
+
+export type NextApiResponseServerIO = NextApiResponse & {
+	socket: Socket & {
+		server: NetServer & {
+			io: SocketIOServer
+		}
+	}
 }
