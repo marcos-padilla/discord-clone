961fecc3e043f8f01fc07a33693c890497dc5b7a
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-09-05 10:11:13 -0400

    feat: edit channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -56,3 +56,66 @@ export async function DELETE(
 		return new NextResponse('Internal Server Error', { status: 500 })
 	}
 }
+
+export async function PATCH(
+	req: Request,
+	{
+		params,
+	}: {
+		params: {
+			channelId: string
+		}
+	}
+) {
+	try {
+		const profile = await getCurrentProfile()
+		if (!profile) {
+			return new NextResponse('Unauthorized', { status: 401 })
+		}
+
+		const { searchParams } = new URL(req.url)
+		const serverId = searchParams.get('serverId')
+
+		if (!serverId || !params.channelId) {
+			return new NextResponse('Missing Info', { status: 400 })
+		}
+
+		const { name, type } = await req.json()
+		if (name === 'general') {
+			return new NextResponse('Name cannot be `general`', {
+				status: 400,
+			})
+		}
+		const server = await prisma.server.update({
+			where: {
+				id: serverId,
+				members: {
+					some: {
+						profileId: profile.id,
+						role: {
+							in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+						},
+					},
+				},
+			},
+			data: {
+				channels: {
+					update: {
+						where: {
+							id: params.channelId,
+							name: {
+								not: 'general',
+							},
+						},
+						data: { name, type },
+					},
+				},
+			},
+		})
+
+		return NextResponse.json(server)
+	} catch (error) {
+		console.log(error)
+		return new NextResponse('Internal Server Error', { status: 500 })
+	}
+}
--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import CreateChannelModal from '@/components/modals/CreateChannelModal'
 import CreateServerModal from '@/components/modals/CreateServerModal'
 import DeleteChannelModal from '@/components/modals/DeleteChannelModal'
 import DeleteServerModal from '@/components/modals/DeleteServerModal'
+import EditChannelModal from '@/components/modals/EditChannelModal'
 import EditServerModal from '@/components/modals/EditServerModal'
 import InviteModal from '@/components/modals/InviteModal'
 import LeaveServerModal from '@/components/modals/LeaveServerModal'
@@ -28,6 +29,7 @@ export default function ModalProvider() {
 			<LeaveServerModal />
 			<DeleteServerModal />
 			<DeleteChannelModal />
+			<EditChannelModal />
 		</>
 	)
 }
